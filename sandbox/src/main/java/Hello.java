5c28a54173ce45208eed9fc1a6de193f39a54e5f
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-09-17 14:57:09 +0300

    Эксперименты с функциями

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
     public static void main(String [] args) {
         System.out.println("Hello world!");
+
+        System.out.println(2 + 2);
+        System.out.println(2 - 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+
+        System.out.println("2 + 2 = " + (2 + 2));
+
     }
 }
