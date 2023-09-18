d5ab9e500558988c492afa23135e393b2210c706
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-09-18 22:51:50 +0300

    Тест для вычисления площади треугольника

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -16,7 +16,8 @@ public class Triangle {
         return this.a + this.b + this.c;
     }
 
- //   public double area(){
- //       return
- //  }
+    public double area(){
+        double p = ((this.a + this.b + this.c) / 2);
+        return Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+   }
 }
