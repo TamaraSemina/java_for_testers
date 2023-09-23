65710c46e82d848c22853e809633093f730c7684
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-09-23 21:33:58 +0300

    Сравнивание квадратов и прямоугольников

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,5 +1,7 @@
 package ru.stqa.geometry.figures;
 
+import java.util.Objects;
+
 public record Rectangle  (
         double a,
         double b
@@ -18,5 +20,19 @@ public record Rectangle  (
 
     private static double rectangleArea(double a, double b) {
         return a * b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return Double.compare(this.a, rectangle.a) == 0 && Double.compare(this.b, rectangle.b) == 0
+                || Double.compare(this.b, rectangle.a) == 0 && Double.compare(this.a, rectangle.b) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
     }
 }
--- sandbox/src/test/java/ru/stqa/geometry/figures/SquareTest.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/SquareTest.java
@@ -26,4 +26,25 @@ public class SquareTest {
             // OK
         }
     }
+
+    @Test
+    void testEquality() {
+        var s1 = new Square(5.0);
+        var s2 = new Square(5.0);
+        Assertions.assertEquals(s1, s2);
+    }
+
+    @Test
+    void testNonEquality() {
+        var s1 = new Square(5.0);
+        var s2 = new Square(4.0);
+        Assertions.assertNotEquals(s1, s2);
+    }
+
+    @Test
+    void testPass() {
+        var s1 = new Square(5.0);
+        var s2 = new Square(5.0);
+        Assertions.assertTrue(s1.equals(s2));
+    }
 }
