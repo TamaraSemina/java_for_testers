a1f5545fb0dcfad405b29449b73cd7e34872b5ab
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-09-23 19:02:03 +0300

    Создан тест cannotCreateTriangleWithNegativeSide

--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTest.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTest.java
@@ -18,4 +18,15 @@ public class TriangleTest {
         double result = a.area();
         Assertions.assertEquals(336, result);
     }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSide () {
+
+        try {
+            new Triangle(-26.0, 30.0, 28.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
+    }
 }
