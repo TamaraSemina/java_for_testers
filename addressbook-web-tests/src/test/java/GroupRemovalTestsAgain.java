9ffd691e75d3f6de56c83f961c3794c13665919c
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-09-26 22:33:04 +0300

    Создан тест GroupCreationTestsAgain

---
+++ addressbook-web-tests/src/test/java/GroupRemovalTestsAgain.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GroupRemovalTestsAgain {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new FirefoxDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void canRemoveGroupAgain() {
+        driver.get("http://localhost/addressbook/");
+        driver.manage().window().setSize(new Dimension(1936, 1048));
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
+        driver.findElement(By.linkText("groups")).click();
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.name("delete")).click();
+        driver.findElement(By.linkText("group page")).click();
+        driver.findElement(By.linkText("Logout")).click();
+    }
+}
