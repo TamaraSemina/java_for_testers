7fd2cd48e348a2b6e3d83395c7dad8f8b8229f8f
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-09-26 21:23:33 +0300

    Создан тест GroupCreationTestsAgain

---
+++ addressbook-web-tests/src/test/java/GroupCreationTestsAgain.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GroupCreationTestsAgain {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new FirefoxDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void canCreateGroupAgain() {
+        driver.get("http://localhost/addressbook/");
+        driver.manage().window().setSize(new Dimension(1936, 1048));
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
+        driver.findElement(By.linkText("groups")).click();
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys("group name");
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).sendKeys("groupheader");
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).sendKeys("group fotter");
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("group page")).click();
+        driver.findElement(By.linkText("Logout")).click();
+    }
+}
