6116739abbb219faeaf52eb11f9bd9cb08f90548
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-10-14 20:46:34 +0300

    5.2. Список файлов в директории

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -18,7 +18,7 @@ public class ContactCreationTests extends TestBase {
         var contact = new ContactData()
                 .withFirstName(randomString(10))
                 .withLastName(randomString(10))
-                .withPhoto("src/test/resources/images/avatar.jpg");
+                .withPhoto(randomFile("src/test/resources/images"));
         app.contacts().createContact(contact);
     }
 
--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -3,6 +3,8 @@ package tests;
 import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.Random;
 
 public class TestBase {
@@ -25,5 +27,12 @@ public class TestBase {
             result = result + (char)('a' + rnd.nextInt(26));
         }
         return result;
+    }
+
+    public static String randomFile(String dir) {
+        var fileNames = new File(dir).list();
+        var rnd = new Random();
+        var index = rnd.nextInt(fileNames.length);
+        return Paths.get(dir, fileNames[index]).toString();
     }
 }
