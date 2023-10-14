eae7b44dcad28edba85d0ca30b9c6779ec426021
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-10-14 16:34:12 +0300

    Обновлен Hello world

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,13 +1,21 @@
+import java.io.File;
+
 public class Hello {
-    public static void main(String [] args) {
-            var x = 1;
-            var y = 0;
-            if ( y == 0) {
-                System.out.println("Division by zero is not allowed");
-            } else {
-                var z = divide(x, y);
-                System.out.println("Hello world!");
-            }
+    public static void main(String[] args) {
+        var x = 1;
+        var y = 0;
+        if (y == 0) {
+            System.out.println("Division by zero is not allowed");
+        } else {
+            var z = divide(x, y);
+            System.out.println("Hello world!");
+        }
+
+        var configFile = new File("sandbox/build.gradle");
+        System.out.println(configFile.getAbsolutePath());
+        System.out.println(configFile.exists());
+
+        System.out.println(new File("").getAbsolutePath());
     }
 
     private static int divide(int x, int y) {
