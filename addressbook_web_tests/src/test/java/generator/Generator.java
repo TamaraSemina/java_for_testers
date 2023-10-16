eb9b44c06ba3bf33a2c5086be266f1e7c425ba21
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-10-16 20:33:13 +0300

    5.6. Сохранение в формате YAML

--- addressbook_web_tests/src/test/java/generator/Generator.java
+++ addressbook_web_tests/src/test/java/generator/Generator.java
@@ -4,6 +4,7 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import common.CommonFunction;
 import model.GroupData;
 
@@ -68,13 +69,16 @@ public class Generator {
 
     private void save(Object data) throws IOException {
         if ("json".equals(format)) {
-            ObjectMapper mapper =  new ObjectMapper();
+            ObjectMapper mapper = new ObjectMapper();
             mapper.enable(SerializationFeature.INDENT_OUTPUT);
             var json = mapper.writeValueAsString(data);
 
             try (var writer = new FileWriter(output)) {
                 writer.write(json);
             }
+        } if ("yaml".equals(format)) {
+            var mapper = new YAMLMapper();
+            mapper.writeValue(new File(output), data);
         } else {
             throw new IllegalArgumentException("Неизвестный формат данных" + format);
         }
