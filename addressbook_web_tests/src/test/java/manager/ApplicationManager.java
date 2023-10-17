4765995f691ded90b65348c7e47f9378b44cd7a7
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-10-17 19:09:55 +0300

    5.8. Описание конфигурации тестового стенда

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -7,13 +7,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Properties;
+
 public class ApplicationManager {
     protected WebDriver driver;
     private LoginHelper session;
     private GroupHelper groups;
     private ContactHelper contacts;
 
-    public void init(String browser) {
+    private Properties properties;
+
+    public void init(String browser, Properties properties) {
+        this.properties = properties;
         if (driver == null) {
             if ("firefox".equals(browser)) {
                 driver = new FirefoxDriver();
@@ -23,9 +28,9 @@ public class ApplicationManager {
                 throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
             }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
+            driver.get(properties.getProperty("web.baseURL"));
             driver.manage().window().setSize(new Dimension(1936, 1048));
-            session().login("admin", "secret");
+            session().login(properties.getProperty("web.username"), properties.getProperty("web.password"));
         }
     }
 
--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -4,7 +4,11 @@ import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Properties;
 import java.util.Random;
 
 public class TestBase {
@@ -13,11 +17,13 @@ public class TestBase {
 
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws IOException {
         if (app == null) {
+            var properties = new Properties();
+            properties.load(new FileReader(System.getProperty("targer", "local.properties")));
             app = new ApplicationManager();
+            app.init(System.getProperty("browser", "firefox"), properties);
         }
-        app.init(System.getProperty("browser", "firefox"));
     }
 
     public static String randomFile(String dir) {
