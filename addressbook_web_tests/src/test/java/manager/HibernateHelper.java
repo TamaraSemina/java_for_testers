28d470751321cad006f911ec19e6621803993837
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-10-23 22:57:58 +0300

    6.2 Object Relational Mapping (ORM) обновление

--- addressbook_web_tests/src/test/java/manager/HibernateHelper.java
+++ addressbook_web_tests/src/test/java/manager/HibernateHelper.java
@@ -1,33 +1,44 @@
 package manager;
 
+import manager.hbm.GroupRecord;
 import model.GroupData;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.tool.schema.Action;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class HibernateHelper extends HelperBase {
-
     private SessionFactory sessionFactory;
 
     public HibernateHelper(ApplicationManager manager) {
         super(manager);
-        SessionFactory sessionFactory = new Configuration()
+        sessionFactory = new Configuration()
 //                        .addAnnotatedClass(Book.class)
-//                        .addAnnotatedClass(Author.class)
-                        // Postgres SQL
-                        .setProperty(AvailableSettings.JAKARTA_JBDC_URL, "jdbc:mysql://localhost/addressbook")
-                        .setProperty(AvailableSettings.JAKARTA_JBDC_USER, "root")
-                        .setProperty(AvailableSettings.JAKARTA_JBDC_PASSWORD, "")
+                        .addAnnotatedClass(GroupRecord.class)
+                        .setProperty(AvailableSettings.URL, "jdbc:mysql://localhost/addressbook")
+                        .setProperty(AvailableSettings.USER, "root")
+                        .setProperty(AvailableSettings.PASS, "")
                         .buildSessionFactory();
     }
 
+    static List<GroupData> convertList(List<GroupRecord> records) {
+        List<GroupData> result = new ArrayList<>();
+        for (var record : records) {
+            result.add(convert(record));
+        }
+        return result;
+    }
+
+    private static GroupData convert(GroupRecord record) {
+        return new GroupData("" + record.id, record.name, record.header, record.footer);
+    }
+
     public List<GroupData> getGroupList() {
-        return sessionFactory.fromSession(session -> {
-            return session.createQuery("from GroupData", GroupData.class).list();
-        })
+        return convertList(sessionFactory.fromSession(session -> {
+            return session.createQuery("from GroupRecord", GroupRecord.class).list();
+        }));
     }
 }
