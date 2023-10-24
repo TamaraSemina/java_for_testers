a9c11a4cc7f8adf823123221930a807be05a9994
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-10-24 20:26:54 +0300

    6.3. ORM: создание и изменение записей в базе данных

--- addressbook_web_tests/src/test/java/manager/hbm/GroupRecord.java
+++ addressbook_web_tests/src/test/java/manager/hbm/GroupRecord.java
@@ -5,9 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.util.Date;
+
 @Entity
 @Table (name = "group_list")
 public class GroupRecord {
+
     @Id
     @Column(name = "group_id")
     public int id;
@@ -17,4 +20,16 @@ public class GroupRecord {
     public String header;
     @Column(name = "group_footer")
     public String footer;
+
+    public Date deprecated = new Date(); /*prisvoenie tekushego znachenia*/
+
+    public GroupRecord() {
+    }
+
+    public GroupRecord(int id, String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
 }
