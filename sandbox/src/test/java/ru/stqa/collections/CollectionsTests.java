16286ee9cd89010f906cdfa58a54715e0f09e52c
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-10-29 20:29:42 +0300

    7.4. Множества (1)

--- sandbox/src/test/java/ru/stqa/collections/CollectionsTests.java
+++ sandbox/src/test/java/ru/stqa/collections/CollectionsTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class CollectionsTests {
 
@@ -20,11 +21,17 @@ public class CollectionsTests {
 
    @Test
    void listTests() {
-        var list = new ArrayList<>(List.of("a", "b", "c"));
-        Assertions.assertEquals(3, list.size());
+        var list = new ArrayList<>(List.of("a", "b", "c", "a"));
+        Assertions.assertEquals(4, list.size());
         Assertions.assertEquals("a", list.get(0));
 
         list.set(0, "d");
         Assertions.assertEquals("d", list.get(0));
+    }
+
+    @Test
+    void setTest() {
+        var set = Set.copyOf(List.of("a", "b", "c", "a"));
+        Assertions.assertEquals(3, set.size());
     }
 }
