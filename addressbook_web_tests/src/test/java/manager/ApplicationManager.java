a936bf9fb081173dda7231f295b0e688eb1ef8d7
Семина Тамара Александровна <moonsoob@yandex.ru>
2023-11-17 17:10:37 +0300

    10.5. Удалённый запуск браузера через Selenium Server

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -5,8 +5,13 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 public class ApplicationManager {
@@ -19,12 +24,23 @@ public class ApplicationManager {
 
     private Properties properties;
 
-    public void init(String browser, Properties properties) {
+    public void init(String browser, Properties properties) throws MalformedURLException {
         this.properties = properties;
         if (driver == null) {
+            var seleniumServer = properties.getProperty("seleniumServer");
             if ("firefox".equals(browser)) {
+                if (seleniumServer != null) {
+                    driver = new RemoteWebDriver(new URL(seleniumServer), new FirefoxOptions());
+                } else {
+                    driver = new FirefoxDriver();
+                }
                 driver = new FirefoxDriver();
             } else if ("edge".equals(browser)) {
+                if (seleniumServer != null) {
+                    driver = new RemoteWebDriver(new URL(seleniumServer), new EdgeOptions());
+                } else {
+                    driver = new EdgeDriver();
+                }
                 driver = new EdgeDriver();
             } else {
                 throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
